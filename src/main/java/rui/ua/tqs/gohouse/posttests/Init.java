9fbba5b1a418fa1829652751b75e1db1a74d80ab
Rui Jesus <r.jesus@ua.pt>
2018-05-28 18:36:16 +0100

    Second commit

---
+++ src/main/java/rui/ua/tqs/gohouse/posttests/Init.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rui.ua.tqs.gohouse.posttests;
+
+/**
+ *
+ * @author rui
+ */
+public class Init {
+    
+}
