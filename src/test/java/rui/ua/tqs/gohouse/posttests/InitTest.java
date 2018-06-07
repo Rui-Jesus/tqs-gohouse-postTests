2104c2c38df181f3f6a43fdb802acf8324ec62a7
Joao Serpa <32497693+joaoserpa@users.noreply.github.com>
2018-06-07 02:44:53 +0100

    Simple selinium test

--- src/test/java/rui/ua/tqs/gohouse/posttests/InitTest.java
+++ src/test/java/rui/ua/tqs/gohouse/posttests/InitTest.java
@@ -5,23 +5,25 @@
  */
 package rui.ua.tqs.gohouse.posttests;
 
-import java.util.concurrent.TimeUnit;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  *
  * @author rui
  */
 public class InitTest {
-    
+
     private WebDriver driver;
     private String baseUrl;
     private static final String DRIVER_PATH = "/home/r.jesus/chromedriver"; 
@@ -30,10 +32,10 @@ public class InitTest {
     public void setUp() throws Exception {
         //Este path é o da VM !!!! 
         //Pra testar localmente trocar para o path onde têm o vosso download do chrome driver
-        System.setProperty("webdriver.chrome.driver", DRIVER_PATH); 
-        
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+
         ChromeOptions options = new ChromeOptions();
-        
+
         // setting headless mode to true.. so there isn't any ui
         options.setHeadless(true);
 
@@ -42,28 +44,90 @@ public class InitTest {
         baseUrl = "http://www.netbeans.org";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-    
+
     @Test
     public void testSimple() throws Exception {
-        
-        System.out.println("It's working"); 
-        
-        // And now use this to visit NetBeans
-        driver.get(baseUrl);
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.netbeans.org");
 
-        // Check the title of the page
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().contains("NetBeans");
-            }
-        });
+        System.out.println("It's working");
 
-        //Close the browser
-        driver.quit();
+        driver.get("http://192.168.160.224:8080/tqs-gohouse-1.0-SNAPSHOT/faces/home.xhtml");
+        try {
+            driver.findElement(By.linkText("Logout")).click();
+
+        } catch (Exception e) {
+        }
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.name("j_idt20:j_idt22")).click();
+        driver.findElement(By.name("j_idt20:j_idt22")).clear();
+        driver.findElement(By.name("j_idt20:j_idt22")).sendKeys("jjpp");
+        driver.findElement(By.name("j_idt20:j_idt24")).click();
+        driver.findElement(By.name("j_idt20:j_idt24")).clear();
+        driver.findElement(By.name("j_idt20:j_idt24")).sendKeys("jjpp");
+        driver.findElement(By.name("j_idt20:j_idt26")).click();
+        driver.findElement(By.name("j_idt20:j_idt26")).clear();
+        driver.findElement(By.name("j_idt20:j_idt26")).sendKeys("jjpp@outlook.com");
+        driver.findElement(By.name("j_idt20:j_idt28")).click();
+        driver.findElement(By.id("j_idt20:0:j_idt28")).click();
+        driver.findElement(By.name("j_idt21:j_idt23")).click();
+        driver.findElement(By.linkText("Adicionar Propriedade")).click();
+        driver.findElement(By.name("j_idt19:j_idt21")).click();
+        driver.findElement(By.name("j_idt19:j_idt21")).clear();
+        driver.findElement(By.name("j_idt19:j_idt21")).sendKeys("1");
+        driver.findElement(By.name("j_idt19:j_idt23")).click();
+        driver.findElement(By.name("j_idt19:j_idt23")).clear();
+        driver.findElement(By.name("j_idt19:j_idt23")).sendKeys("1");
+        driver.findElement(By.id("j_idt19")).click();
+        driver.findElement(By.name("j_idt19:j_idt25")).click();
+        driver.findElement(By.name("j_idt19:j_idt25")).clear();
+        driver.findElement(By.name("j_idt19:j_idt25")).sendKeys("1");
+        driver.findElement(By.id("j_idt19")).click();
+        driver.findElement(By.id("j_idt19")).click();
+        driver.findElement(By.name("j_idt19:j_idt27")).click();
+        new Select(driver.findElement(By.name("j_idt19:j_idt27"))).selectByVisibleText("Apartamento");
+
+        driver.findElement(By.linkText("Minha Conta")).click();
+        driver.findElement(By.linkText("Logout")).click();
     }
-    
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
